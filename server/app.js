677f11a8045ca6940c39c6c891ea13d3d44c5bf7
Fredi tansari <freditansari@gmail.com>
2019-05-16 14:19:54 +0700

    prepare deployment 3

--- server/app.js
+++ server/app.js
@@ -42,6 +42,6 @@ app.use(passport.initialize());
 
 
 
-const PORT=process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
