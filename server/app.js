23dec0e31a3b5ab8982f76e83451fa3478617511
Fredi tansari <freditansari@gmail.com>
2019-05-16 14:11:06 +0700

    prepare deployment 3

--- server/app.js
+++ server/app.js
@@ -11,7 +11,8 @@ const app= express();
 
 // import routes:
 const users = require('./routes/users');
-const expenses = require('./routes/expenses')
+const expenses = require('./routes/expenses');
+const path =require('path')
 
 
 const db = require('./config/mongodb').mongoURI;
@@ -20,10 +21,18 @@ app.use(bodyParser.json());
 
 mongoose.connect(db, { useFindAndModify: false, useNewUrlParser:true }).then(()=>console.log('Monggo db connecteed')).catch(err=>console.log(err));
 
-app.get('/', (req, res)=>res.send('hello world'));
+
 
 app.use('/api/users', users);
 app.use('/api/expenses', expenses );
+
+//Serve Static assets in productions
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    })
+}
 
 // Passport Config
 require('./config/passport')(passport);
@@ -33,6 +42,6 @@ app.use(passport.initialize());
 
 
 
-const port = process.env.PORT || 5000;
+const PORT=process.env.PORT||5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${PORT}`));
