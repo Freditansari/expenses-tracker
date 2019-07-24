530d59e4ba4bf65d2c508a6348c0de3183741a52
Freditansari <Freditansari@users.noreply.github.com>
2019-07-24 15:32:58 +0700

    Update mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,7 +1,7 @@
 // mongodb://<dbuser>:<dbpassword>@ds261929.mlab.com:61929/expenses
 
 module.exports = {
-    mongoURI: 'mongodb://fredy:testpassword01@ds261929.mlab.com:61929/expenses',
+    mongoURI: 'mongodb://fredy:testpassword01@ds261929.mlab.com:61929/banana',
     secretOrKey: 'secret'
   };
   
